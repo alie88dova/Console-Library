c72fb3d84627443083bbdedab9a44d635b5a2896
A_lie <73252601+alie88dova@users.noreply.github.com>
2024-07-20 20:50:45 +0300

    Commit 20:50 20-07-2024
    Начало работы над заданием

---
+++ Models/Book.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+class Book:
+    pass
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def main():
+    print("Начало работы 20:47 20-07-2024")
+
+
+if __name__ == '__main__':
+    main()
