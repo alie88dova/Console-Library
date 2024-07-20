ad6749385a62a3e225072bbd18e9448ae25287d1
A_lie <73252601+alie88dova@users.noreply.github.com>
2024-07-20 22:39:10 +0300

    Update class Book
    Добавлены update_<(title, year, author)>, get_json_file, book_give, book_return

--- Models/Book.py
+++ Models/Book.py
@@ -11,8 +11,9 @@ class Status(Enum):
 
 class Book:
     """
-    Хранит информацию об отдельном экзэмпляре книги из библиотеки
+    Хранит информацию об отдельном экземпляре книги из библиотеки
     id - str уникальный идентификатор книги
+    author - str автор книги, если автора нет хранит 'Автора нет'
     title - str хранит название книги
     year - int хранит год издания книги
     status - str хранит информацию о статусе книги (Может принимать значения только из Status)
@@ -47,3 +48,49 @@ class Book:
         string = salt + str(datetime.now())
 
         return sha256(string.encode()).hexdigest()
+
+    def update_title(self, title: str):
+        """
+        Изменяет значение title на переданное
+        :param title:
+        """
+        self.title = title
+
+    def update_year(self, year: str):
+        """
+        Изменяет значение year на переданное
+        :param year:
+        """
+        self.year = year
+
+    def update_author(self, author="Автора нет"):
+        """
+        Изменяет значение author на переданное
+        :param author:
+        """
+        self.author = author
+
+    def book_return(self):
+        """
+        Изменяет статус книги на 'В наличии'
+        """
+        self.status = Status.available
+
+    def book_give(self):
+        """
+        Изменяет статус книги на 'Выдана'
+        """
+        self.status = Status.given
+        self.date_last_given = datetime.now()
+
+    def get_json_file(self) -> dict:
+        """Возвращает словарь готовый к упаковке в json"""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "status": self.status,
+            "date_add": self.date_add,
+            "date_last_given": self.date_last_given,
+        }
